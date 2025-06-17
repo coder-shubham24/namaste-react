68f3d21e1b5aa81f9677f8149a02d4840c1e9c08
Vicky122424 <sp60928@gmail.com>
2025-06-17 13:58:38 +0530

    react components

--- app.js
+++ app.js
@@ -1,13 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const Title = () =>(
+const Title = () => (
+  <div>
     <h1>This is the title</h1>
-)
+    <button onClick={() => alert("button clicked")}>Click here</button>
+  </div>
+);
 
-const root = ReactDOM.createRoot(document.getElementById('root'))
+// React components
 
-root.render(<Title/>)
+// functional comp
+const MyComp = () => (
+  <div>
+    <Title/>
+    <h1>Thsi is our 2nd component</h1>
+  </div>
+);
 
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
-
+root.render(
+    <MyComp/>
+);
