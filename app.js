4c83311837de1e479282cbf93f2b057e405731d1
Vicky122424 <sp60928@gmail.com>
2025-06-17 13:18:13 +0530

    learned about nodemodule

--- app.js
+++ app.js
@@ -1,2 +1,13 @@
-const parent = createReactElement(document.getElementById('root'))
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const Title = () =>(
+    <h1>This is the title</h1>
+)
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+
+root.render(<Title/>)
+
+
 
